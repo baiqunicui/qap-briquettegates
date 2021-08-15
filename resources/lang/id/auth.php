ddaca73891971027228b741e6d4327bcda29986e
Laravel Daily <info@laraveldaily.com>
2021-08-15 19:49:57 +0000

    QuickAdminPanel automatic commit

---
+++ resources/lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'ID pengguna atau password tidak sah.',
+    'password' => 'Password yang anda masukkan salah',
+    'throttle' => 'Terlalu banyak percobaan log masuk.  Sila coba semula dalam :seconds saat.',
+];
